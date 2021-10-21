65f54788458412227fefc10d8dccedd80b67a58a
latifvaid <71100717+latifvaid@users.noreply.github.com>
2021-10-21 04:43:59 -0500

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -1,2 +1,28 @@
+#include "grader.h"
 
 
+class chessboard {
+    private:
+        int chessBoard[8][8];
+        
+    public:
+        int testPlace (int x, int y, testColor c, testPiece p) {
+
+        }
+
+        int testGet (int x, int y, testColor &c, testPiece &p) {
+
+        }
+
+        int place (int x, int y, color c, piece p) {
+
+        }
+
+        int get (int x, int y, color &c, piece &p) {
+
+        }
+
+        iterator begin() {};
+        iterator end() {};
+
+};
